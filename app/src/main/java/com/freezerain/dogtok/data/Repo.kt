26ee1ac58dbe4e3875b85b9dba97500497d1598a
Freeze <rain-ilia@rambler.ru>
2023-11-17 20:31:43 +0100

    cleanup old implementation

--- app/src/main/java/com/freezerain/dogtok/data/Repo.kt
+++
@@ -1,8 +0,0 @@
-package com.freezerain.dogtok.data
-
-import android.graphics.Bitmap
-import kotlinx.coroutines.channels.ReceiveChannel
-
-interface Repo {
-    val imageProducer : ReceiveChannel<Bitmap>
-}
